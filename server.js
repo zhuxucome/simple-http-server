e042c28a478ae83a7153bb162103293194b8dacf
zhuxu <zhuxucome@gmail.com>
2023-04-21 14:09:59 +0800

    add routing function

--- server.js
+++ server.js
@@ -6,10 +6,20 @@ const port = process.argv[2] || 8888;
 
 // 创建一个 http 服务器
 const server = http.createServer((req, res) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain;charset=utf-8');
-    res.end('你好呀！');
-});
+    if (req.url === '/') {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'text/plain;charset=utf-8');
+        res.end('你好呀，这是首页。');
+    } else if (req.url === '/about') {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'text/plain;charset=utf-8');
+        res.end('这是「关于我们」页面。');
+    } else {
+        res.statusCode = 404;
+        res.setHeader('Content-Type', 'text/plain;charset=utf-8');
+        res.end('404 页面不存在！');
+    }
+  });
 
 // 监听指定端口，服务器启动后输出对应的提示信息
 server.listen(port, () => {
